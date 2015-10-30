de366ea21f6a619f5be104bf869e136b128c749b
Peter Lacko <placko@redhat.com>
2015-10-30 15:53:04 +0100

    Simple repo without api added.

--- pulp_smash/resources/helper.py
+++ pulp_smash/resources/helper.py
@@ -4,10 +4,44 @@ from __future__ import unicode_literals
 
 import string
 import random
+import time
+import requests
 
 paths = {
     'REPOSITORY_PATH': '/pulp/api/v2/repositories/',
     'TASK_PATH': '/pulp/api/v2/tasks/',
+}
+
+#  Repository related variables
+ERROR_KEYS = {
+    '_href',
+    'error',
+    'error_message',
+    'exception',
+    'http_status',
+    'traceback',
+}
+
+# Task related variables
+REPORT_KEYS = {
+    'result',
+    'error',
+    'spawned_tasks',
+}
+
+task_states = {
+    'TASK_ERROR_STATES': {
+        'error',
+        'timed out',
+    },
+    'TASK_SUCCESS_STATES': {
+        'finished',
+    },
+    'TASK_RUNNING_STATES': {
+        'running',
+        'suspended',
+        'waiting',
+    }
 }
 
 
@@ -16,3 +50,46 @@ def get_random_string(length=15):
     return ''.join(
         random.choice(string.ascii_lowercase) for i in range(0, length)
     )
+
+
+def _wait_for_task(task, cfg, timeout, frequency):
+    """Wait for single task to finish its execution on server.
+    :param task: Dictionary containtin task_id and path to task
+        on pulp server.
+    :param timeout: Timeout in seconds for each task to complete.
+    :param frequency: Task polling frequency in seconds.
+    """
+    task_timeout = time.time() + timeout
+    while time.time() <= task_timeout:
+        time.sleep(frequency)
+        response = requests.get(
+            (
+                cfg.base_url +
+                paths['TASK_PATH'] +
+                '{}/'.format(task["task_id"])
+            ),
+            **cfg.get_requests_kwargs()
+        )
+        response.raise_for_status()
+        # task finished (with success or failure)
+        if (
+            response.json()['state']
+            in task_states['TASK_ERROR_STATES'] |
+            task_states['TASK_SUCCESS_STATES']
+        ):
+            return response
+    # task probably timed out
+
+
+def wait_for_tasks(report, cfg, timeout=120, frequency=0.5):
+    """Wait for all populated tasks to finish.
+    :param report: Call response -- report -- with list of populated tasks.
+    :param timeout: Timeout in seconds for each task to complete.
+    :param frequency: Task polling frequency in seconds.
+    :return: List of tasks and their states
+    """
+    responses = []
+    # all(key in report.json().keys() for key in REPORT_KEYS):
+    for task in report.json()["spawned_tasks"]:
+        responses.append(_wait_for_task(task, cfg, timeout, frequency))
+    return responses
--- pulp_smash/tests/platform/api_v2/simple_repo_test.py
+++ pulp_smash/tests/platform/api_v2/simple_repo_test.py
@@ -4,7 +4,8 @@ from __future__ import unicode_literals
 
 import requests
 
-from pulp_smash.resources.helper import get_random_string
+from pulp_smash.resources.helper import paths, ERROR_KEYS
+from pulp_smash.resources.helper import get_random_string, wait_for_tasks
 from pulp_smash.config import get_config
 from unittest2 import TestCase
 
@@ -18,14 +19,18 @@ class RepoCreateSuccessTestCase(TestCase):
         cls.cfg = get_config()
         cls.repo_id = get_random_string()
         cls.last_response = requests.post(
-            self.cfg.base_url + paths['REPOSITORY_PATH'],
-            json=cls.repo_id,
-            **self.cfg.get_requests_kwargs()
+            cls.cfg.base_url + paths['REPOSITORY_PATH'],
+            json={'id': cls.repo_id},
+            **cls.cfg.get_requests_kwargs()
         )
 
     def test_status_code(self):
         """Test if Create repo returned 201."""
-        self.assertEqual(self.last_response.status_code, 201)
+        self.assertEqual(
+            self.last_response.status_code,
+            201,
+            self.last_response.json()
+        )
 
     def test_correct_id(self):
         """Test if response contain correct repo id."""
@@ -34,3 +39,210 @@ class RepoCreateSuccessTestCase(TestCase):
             self.repo_id,
             self.last_response.json()
         )
+
+    @classmethod
+    def tearDownClass(cls):
+        """Delete previously created repository."""
+        cls.last_response = requests.delete(
+            cls.cfg.base_url +
+            paths['REPOSITORY_PATH'] + '{}/'.format(cls.repo_id),
+            **cls.cfg.get_requests_kwargs()
+        )
+        cls.last_response.raise_for_status()
+        wait_for_tasks(cls.last_response, cls.cfg)
+
+
+class RepoCreateMissingIdTestCase(TestCase):
+    """Test create repo functionality with missing required data keys(id)."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Create Repository with missing required data key."""
+        cls.cfg = get_config()
+        cls.repo_description = get_random_string()
+        cls.last_response = requests.post(
+            cls.cfg.base_url + paths['REPOSITORY_PATH'],
+            json={'description': cls.repo_description},
+            **cls.cfg.get_requests_kwargs()
+        )
+
+    def test_status_code(self):
+        """Check that request returned 400: invalid parameters."""
+        self.assertEqual(
+            self.last_response.status_code,
+            400,
+            self.last_response.json()
+        )
+
+    def test_body(self):
+        """Test if request returned correct body."""
+        self.assertLessEqual(
+            ERROR_KEYS,
+            set(self.last_response.json().keys()),
+            self.last_response.json()
+        )
+
+
+class RepoExistsTestCase(TestCase):
+    """Test if created repo exists on server."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Create repository on server with id, description and display_name,
+        test correct status code and get it.
+        """
+        cls.cfg = get_config()
+        # string representing id, disp. name and description of repo
+        cls.repo_id = get_random_string()
+        cls.last_response = requests.post(
+            cls.cfg.base_url + paths['REPOSITORY_PATH'],
+            json={
+                'id': cls.repo_id,
+                'display_name': cls.repo_id,
+                'description': cls.repo_id
+            },
+            **cls.cfg.get_requests_kwargs()
+        )
+        cls.last_response.raise_for_status()
+        cls.last_response = requests.get(
+            cls.cfg.base_url +
+            paths['REPOSITORY_PATH'] +
+            '{}/'.format(cls.repo_id),
+            **cls.cfg.get_requests_kwargs()
+        )
+
+    def test_status_code(self):
+        """Test if server returned 200."""
+        self.assertEqual(
+            self.last_response.status_code,
+            200,
+            self.last_response.json()
+        )
+
+    def test_keys(self):
+        """Test if repo has all attributes: id, description and display_name.
+        """
+        self.assertLessEqual(
+            {'id', 'display_name', 'description'},
+            set(self.last_response.json().keys())
+        )
+
+    def test_values(self):
+        """Test if required values are correct."""
+        self.assertEqual(
+            {
+                self.last_response.json()['id'],
+                self.last_response.json()['description'],
+                self.last_response.json()['display_name']
+            },
+            {self.repo_id, self.repo_id, self.repo_id},
+            self.last_response.json()
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        """Delete previously created repository."""
+        cls.last_response = requests.delete(
+            cls.cfg.base_url +
+            paths['REPOSITORY_PATH'] + '{}/'.format(cls.repo_id),
+            **cls.cfg.get_requests_kwargs()
+        )
+        cls.last_response.raise_for_status()
+        wait_for_tasks(cls.last_response, cls.cfg)
+
+
+class RepoDeleteTestCase(TestCase):
+    """Testing succesfull repo deletion."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Create, delete and get repository. Raise if error occured."""
+        cls.cfg = get_config()
+        cls.repo_id = get_random_string()
+        cls.last_response = requests.post(
+            cls.cfg.base_url + paths['REPOSITORY_PATH'],
+            json={'id': cls.repo_id},
+            **cls.cfg.get_requests_kwargs()
+        )
+        cls.last_response.raise_for_status()
+        cls.last_response = requests.delete(
+            cls.cfg.base_url +
+            paths['REPOSITORY_PATH'] + '{}/'.format(cls.repo_id),
+            **cls.cfg.get_requests_kwargs()
+        )
+        cls.last_response.raise_for_status()
+        cls.last_response = requests.get(
+            cls.cfg.base_url +
+            paths['REPOSITORY_PATH'] +
+            '{}/'.format(cls.repo_id),
+            **cls.cfg.get_requests_kwargs()
+        )
+
+    def test_status_code(self):
+        """Test if request on deleted repo returned 404, Not Found."""
+        self.assertEqual(
+            self.last_response.status_code,
+            404,
+            self.last_response.json()
+        )
+
+    def test_body(self):
+        """Test if body contains all data keys."""
+        self.assertLessEqual(
+            ERROR_KEYS,
+            set(self.last_response.json().keys()),
+            self.last_response.json()
+        )
+
+
+class RepoUpdateTestCase(TestCase):
+    """Create and then update repo. Test if updates were correctly applied."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Create and update repo, get repo."""
+        cls.cfg = get_config()
+        cls.repo_id = get_random_string()
+        cls.last_response = requests.post(
+            cls.cfg.base_url + paths['REPOSITORY_PATH'],
+            json={'id': cls.repo_id},
+            **cls.cfg.get_requests_kwargs()
+        )
+        cls.last_response.raise_for_status()
+        cls.delta = {
+            'display_name': cls.repo_id,
+            'description': cls.repo_id,
+        }
+        cls.last_response = requests.put(
+            cls.cfg.base_url +
+            paths['REPOSITORY_PATH'] +
+            '{}/'.format(cls.repo_id),
+            json=cls.delta,
+            **cls.cfg.get_requests_kwargs()
+        )
+
+    def test_status_code(self):
+        """Test that status code of update repo call is 200."""
+        self.assertEqual(
+            self.last_response.status_code,
+            200,
+            self.last_response.json()
+        )
+
+    def test_keys(self):
+        """Test that repository description and display names are correct."""
+        self.assertLessEqual(
+            {'id', 'display_name', 'description'},
+            set(self.last_response.json()['result'].keys())
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        """Delete previously created repository."""
+        cls.last_response = requests.delete(
+            cls.cfg.base_url +
+            paths['REPOSITORY_PATH'] + '{}/'.format(cls.repo_id),
+            **cls.cfg.get_requests_kwargs()
+        )
+        cls.last_response.raise_for_status()
+        wait_for_tasks(cls.last_response, cls.cfg)
